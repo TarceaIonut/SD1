293abfabf1a0934edb076c614671bfccdb35b149
Tarcea Ionut <tudor.ionut.tarcea@gmail.com>
2026-04-20 10:50:01 +0300

    sing

--- Hospital/obj/Debug/net10.0/Hospital.AssemblyInfo.cs
+++ Hospital/obj/Debug/net10.0/Hospital.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hospital")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e5212921640f9778078cdd8a2578046d748fa47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6b046a3eb64dea2a2ec14348f756315174b6ff5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hospital")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hospital")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
